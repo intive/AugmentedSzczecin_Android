a573c8986e0273a8263bad8802c33b3899fd41d3
dbaranek <dbaranek@wi.zut.edu.pl>
2015-03-21 22:02:38 +0100

    [FIX] Applying suggested changes

--- app/src/main/java/com/blstream/as/ArFragment.java
+++ app/src/main/java/com/blstream/as/ArFragment.java
@@ -1,16 +1,20 @@
 package com.blstream.as;
 
 
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.FrameLayout;
 
 
 public class ArFragment extends Fragment {
-
+    private static final String TAG = "ArFragment";
+    private Camera camera;
+    private CameraPreview cameraPreview;
     public static ArFragment newInstance() {
         return new ArFragment();
     }
@@ -29,5 +33,50 @@ public class ArFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ar, container, false);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        safeCameraOpenInView();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        releaseCamera();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        releaseCamera();
+    }
+    private boolean safeCameraOpenInView() {
+        // Create an instance of Camera
+        try {
+            camera = null;
+            camera = Camera.open(); // attempt to get a Camera instance
+            // Create our Preview view and set it as the content of our activity.
+            cameraPreview = new CameraPreview(getActivity(), camera);
+            FrameLayout preview = (FrameLayout) getView().findViewById(R.id.cameraSurface);
+            preview.addView(cameraPreview);
+        }
+        catch (Exception e) {
+            Log.e(TAG,e.getMessage());
+            return false;
+        }
+        return true;
+    }
+    private void releaseCamera() {
+        if (camera != null) {
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }
+        if (cameraPreview != null) {
+            FrameLayout preview = (FrameLayout) getView().findViewById(R.id.cameraSurface);
+            preview.removeView(cameraPreview);
+            cameraPreview = null;
+        }
     }
 }
---
+++ app/src/main/java/com/blstream/as/CameraPreview.java
@@ -0,0 +1,69 @@
+package com.blstream.as;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+/** A basic Camera preview class */
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private static final String TAG = "CameraPreview";
+    private SurfaceHolder surfaceHolder;
+    private Camera camera;
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        } catch (IOException e) {
+            Log.e(TAG,e.getMessage());
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // empty. Take care of releasing the Camera preview in your activity.
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+        if (surfaceHolder.getSurface() == null){
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            camera.stopPreview();
+        } catch (Exception e){
+            Log.e(TAG,e.getMessage());
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+
+        // start preview with new settings
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+
+        } catch (Exception e){
+            Log.e(TAG,e.getMessage());
+        }
+    }
+}
