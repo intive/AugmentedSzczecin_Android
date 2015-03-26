057ea18540b61fb44ba3853f56af04ef534094b6
kstraszewski <kondek96@gmail.com>
2015-03-26 13:23:16 +0100

    [FIX] Dodaj POI button

--- app/src/main/java/com/blstream/as/MockDialog.java
+++ app/src/main/java/com/blstream/as/MockDialog.java
@@ -1,9 +1,11 @@
 package com.blstream.as;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.gms.maps.model.LatLng;
