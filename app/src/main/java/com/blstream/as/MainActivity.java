cef854d7c3a6c97eb3537dff7027f8fefa761e82
asia-st <joannastachiewicz@gmail.com>
2015-05-22 10:43:22 +0200

    [FIX] moving to start screen after blocking screen

--- app/src/main/java/com/blstream/as/MainActivity.java
+++ app/src/main/java/com/blstream/as/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 
 import com.blstream.as.fragment.ActionBarConnector;
@@ -44,8 +45,8 @@ public class MainActivity extends ActionBarActivity implements ActionBarConnecto
     public void onResume() {
         super.onResume();
 
-        if (getFragmentManager().getBackStackEntryCount() > 0) {
-            getFragmentManager().popBackStackImmediate(getFragmentManager().getBackStackEntryAt(0).getId(), android.support.v4.app.FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        if (getSupportFragmentManager().getBackStackEntryCount() > 1){
+            getSupportFragmentManager().popBackStackImmediate(getSupportFragmentManager().getBackStackEntryAt(0).getId(), FragmentManager.POP_BACK_STACK_INCLUSIVE);
         }
     }
 
